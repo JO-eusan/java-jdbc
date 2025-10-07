0afec9158a6ca1c2defc22817a1d084fe34121a5
San <goodsan0330@gmail.com>
2025-10-07 14:14:23 +0900

    [1단계 - JDBC 라이브러리 구현하기] 사나(조은산) 미션 제출합니다. (#1009)
    * chore: Poling 오타 수정 (#928)
    
    * study: transaction 학습
    
    * docs: 테스트 의존성 추가
    
    * feat: JdbcTemplate 쿼리 실행 로직 추가
    
    * feat: UserDao의 의존성을 JdbcTemplate로 변경
    
    * refactor: UserHistoryDao의 의존성을 JdbcTemplate로 변경
    
    * refactor: JdbcTemplate 파라미터 바인딩 메서드 분리
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

---
+++ jdbc/README.md
@@ -0,0 +1,7 @@
+## JDBC 라이브러리 구현하기
+
+### 1단계 - JDBC 라이브러리 구현하기
+
+- [x] JdbcTemplate 클래스에 자주 사용하는 로직 분리
+- [x] app 모듈의 UserDao 클래스를 JdbcTemplate 사용하도록 변경
+- [x] UserDaoTest 모두 통과
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,16 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,76 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... parameters) {
+        update(sql, bindParameters(parameters));
+    }
+
+    public void update(String sql, Consumer<PreparedStatement> parameterSetter) {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            parameterSetter.accept(pstmt);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(String sql, Function<ResultSet, T> rowMapper, Object... parameters) {
+        return query(sql, bindParameters(parameters), rowMapper);
+    }
+
+    public <T> List<T> query(String sql, Consumer<PreparedStatement> parameterSetter,
+                             Function<ResultSet, T> rowMapper) {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            parameterSetter.accept(pstmt);
+
+            try (final var rs = pstmt.executeQuery()) {
+                final var results = new ArrayList<T>();
+                while (rs.next()) {
+                    results.add(rowMapper.apply(rs));
+                }
+                return results;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, Consumer<PreparedStatement> parameterSetter,
+                                Function<ResultSet, T> rowMapper) {
+        var results = query(sql, parameterSetter, rowMapper);
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results.getFirst();
+    }
+
+    public <T> T queryForObject(String sql, Function<ResultSet, T> rowMapper, Object... parameters) {
+        var results = query(sql, rowMapper, parameters);
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results.getFirst();
+    }
+
+    private Consumer<PreparedStatement> bindParameters(Object... parameters) {
+        return preparedStatement -> {
+            for (int i = 0; i < parameters.length; i++) {
+                try {
+                    preparedStatement.setObject(i + 1, parameters[i]);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
     }
 }
