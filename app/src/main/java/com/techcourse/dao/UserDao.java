652f56496b682a62cd953b900a5e3cc7646f6e89
San <goodsan0330@gmail.com>
2025-10-10 17:22:02 +0900

    refactor: JdbcTemplate 파라미터 객체화 및 도메인 의존성 제거 (#1063)

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,8 +1,8 @@
 package com.techcourse.dao;
 
+import com.interface21.jdbc.core.BeanPropertyRowMapper;
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.domain.User;
-import java.sql.ResultSet;
 import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
@@ -13,6 +13,7 @@ public class UserDao {
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
     private final JdbcTemplate jdbcTemplate;
+    private final BeanPropertyRowMapper<User> userMapper = new BeanPropertyRowMapper<>(User.class);
 
     public UserDao(final DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -34,29 +35,16 @@ public class UserDao {
 
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(sql, this::mapRowToUser);
+        return jdbcTemplate.query(sql, userMapper);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, this::mapRowToUser, id);
+        return jdbcTemplate.queryForObject(sql, userMapper, id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(sql, this::mapRowToUser, account);
-    }
-
-    private User mapRowToUser(ResultSet resultSet) {
-        try {
-            return new User(
-                    resultSet.getLong("id"),
-                    resultSet.getString("account"),
-                    resultSet.getString("password"),
-                    resultSet.getString("email")
-            );
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
+        return jdbcTemplate.queryForObject(sql, userMapper, account);
     }
 }
